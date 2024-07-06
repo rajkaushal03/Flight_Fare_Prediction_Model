3e72f70df4255ec2c97044af400ae673303139f4
Swayam Kaushal <101446225+rajkaushal03@users.noreply.github.com>
2024-07-06 17:51:56 +0530

    first

--- app.py
+++ app.py
@@ -363,7 +363,7 @@ def predict():
 
 
 
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+from waitress import serve
 
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=8080)
